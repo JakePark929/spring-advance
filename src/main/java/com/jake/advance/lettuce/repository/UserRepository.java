6ab3dc015f200678d42768e0de4e696bbf559ba2
epike <epikeran@gmail.com>
2024-01-30 11:06:33 +0900

    chore: Delete lettuce service

--- src/main/java/com/jake/advance/lettuce/repository/UserRepository.java
+++ src/main/java/com/jake/advance/lettuce/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.jake.advance.lettuce.repository;
 
 import com.jake.advance.lettuce.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/jake/advance/lettuce/service/UserService.java
+++ src/main/java/com/jake/advance/lettuce/service/UserService.java
@@ -16,44 +16,44 @@ import static com.jake.advance._core.config.CacheConfig.CACHE1;
 @RequiredArgsConstructor
 @Service
 public class UserService {
-    private final UserRepository userRepository;
-    private final RedisHashUserRepository redisHashUserRepository;
-    private final RedisTemplate<String, User> userRedisTemplate;
-    private final RedisTemplate<String, Object> objectRedisTemplate;
-
-    public User getUser(Long id) {
-        var key = "users:%d".formatted(id);
-
-        // 1. cache get
-        var cachedUser = objectRedisTemplate.opsForValue().get(key);
-        if (cachedUser != null) {
-            return (User) cachedUser;
-        }
-
-        // 2. else dv -> cache set
-        final User user = userRepository.findById(id).orElseThrow();
-        objectRedisTemplate.opsForValue().set(key, user, Duration.ofSeconds(10));
-
-        // 3. return
-        return user;
-    }
-
-    public RedisHashUser getRedisHashUser(Long id) {
-        return redisHashUserRepository.findById(id).orElseGet(() -> {
-            final User user = userRepository.findById(id).orElseThrow();
-
-            return redisHashUserRepository.save(RedisHashUser.builder()
-                    .id(user.getId())
-                    .name(user.getName())
-                    .email(user.getEmail())
-                    .createdAt(user.getCreatedAt())
-                    .updatedAt(user.getUpdatedAt())
-                    .build());
-        });
-    }
-
-    @Cacheable(cacheNames = CACHE1, key = "'user:' + #id")
-    public User getSpringCacheUser(Long id) {
-        return userRepository.findById(id).orElseThrow();
-    }
+//    private final UserRepository userRepository;
+//    private final RedisHashUserRepository redisHashUserRepository;
+//    private final RedisTemplate<String, User> userRedisTemplate;
+//    private final RedisTemplate<String, Object> objectRedisTemplate;
+//
+//    public User getUser(Long id) {
+//        var key = "users:%d".formatted(id);
+//
+//        // 1. cache get
+//        var cachedUser = objectRedisTemplate.opsForValue().get(key);
+//        if (cachedUser != null) {
+//            return (User) cachedUser;
+//        }
+//
+//        // 2. else dv -> cache set
+//        final User user = userRepository.findById(id).orElseThrow();
+//        objectRedisTemplate.opsForValue().set(key, user, Duration.ofSeconds(10));
+//
+//        // 3. return
+//        return user;
+//    }
+//
+//    public RedisHashUser getRedisHashUser(Long id) {
+//        return redisHashUserRepository.findById(id).orElseGet(() -> {
+//            final User user = userRepository.findById(id).orElseThrow();
+//
+//            return redisHashUserRepository.save(RedisHashUser.builder()
+//                    .id(user.getId())
+//                    .name(user.getName())
+//                    .email(user.getEmail())
+//                    .createdAt(user.getCreatedAt())
+//                    .updatedAt(user.getUpdatedAt())
+//                    .build());
+//        });
+//    }
+//
+//    @Cacheable(cacheNames = CACHE1, key = "'user:' + #id")
+//    public User getSpringCacheUser(Long id) {
+//        return userRepository.findById(id).orElseThrow();
+//    }
 }
