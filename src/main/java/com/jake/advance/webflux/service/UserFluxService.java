1be4ae11ec954b8dc9ba59ba503082eb01c18b90
epike <epikeran@gmail.com>
2024-01-31 10:53:22 +0900

    feat: Reactive redis practice

--- src/main/java/com/jake/advance/webflux/service/UserFluxService.java
+++ src/main/java/com/jake/advance/webflux/service/UserFluxService.java
@@ -3,15 +3,20 @@ package com.jake.advance.webflux.service;
 import com.jake.advance.webflux.domain.User;
 import com.jake.advance.webflux.repository.UserR2dbcRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @RequiredArgsConstructor
 @Service
 public class UserFluxService {
-//    private final UserFluxRepository userFluxRepository;
+    //    private final UserFluxRepository userFluxRepository;
     private final UserR2dbcRepository userR2dbcRepository;
+
+    private final ReactiveRedisTemplate<String, User> reactiveRedisTemplate;
 
     public Mono<User> create(String name, String email) {
         return userR2dbcRepository.save(
@@ -20,6 +25,30 @@ public class UserFluxService {
                         .email(email)
                         .build()
         );
+    }
+
+    public Flux<User> findAll() {
+        return userR2dbcRepository.findAll();
+    }
+
+    private String getUserCacheKey(Long id) {
+        return "users:%d".formatted(id);
+    }
+
+    public Mono<User> findById(Long id) {
+        // 1. redis 조회
+        // 2. 값이 존재하면 응답을 하고
+        // 3. 없으면 DB에 질의 하고 그 결과를 redis 에 저장하는 흐름
+        return reactiveRedisTemplate.opsForValue()
+                .get(getUserCacheKey(id))
+                .switchIfEmpty(
+                        userR2dbcRepository.findById(id)
+                                .flatMap(u -> reactiveRedisTemplate.opsForValue()
+                                        .set(getUserCacheKey(id), u, Duration.ofSeconds(30))
+                                        .then(Mono.just(u))
+                                )
+
+                );
     }
 
     public Mono<User> update(Long id, String name, String email) {
@@ -32,17 +61,17 @@ public class UserFluxService {
                     u.setEmail(email);
 
                     return userR2dbcRepository.save(u);
-                });
+                })
+                .flatMap(u -> reactiveRedisTemplate.unlink(getUserCacheKey(id)).then(Mono.just(u)));
     }
 
-    public Flux<User> findAll() {
-        return userR2dbcRepository.findAll();
+    public Mono<Void> deleteById(Long id) {
+        return userR2dbcRepository.deleteById(id)
+                .then(reactiveRedisTemplate.unlink(getUserCacheKey(id)))
+                .then(Mono.empty());
     }
 
-    public Mono<User> findById(Long id) {
-        return userR2dbcRepository.findById(id);
+    public Mono<Void> deleteByName(String name) {
+        return userR2dbcRepository.deleteByName(name);
     }
-
-    public Mono<Void> deleteById(Long id) { return userR2dbcRepository.deleteById(id); }
-    public Mono<Void> deleteByName(String name) { return userR2dbcRepository.deleteByName(name); }
 }
